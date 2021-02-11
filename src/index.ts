9e1f5fe4fad310661af877b3ac8e32b35d3bb967
Mauro Pompilio <hackers.are.rockstars@gmail.com>
2021-02-11 10:29:14 +0000

    Debug config

--- src/index.ts
+++ src/index.ts
@@ -8,81 +8,88 @@ interface LinkUserResponseBody {
 }
 
 (async () => {
-  // Configure OAuth client library
-  const enodeIssuer = await Issuer.discover(config.get("oauthUrl"));
-  const client = new enodeIssuer.Client({
-    client_id: config.get("clientId"),
-    client_secret: config.get("clientSecret"),
-    redirect_uris: [config.get("callbackUrl")],
-    response_types: ["code"],
-  });
-
-  app.get("/link", async (req, res) => {
-    const user = req.session!.user;
-
-    // Create an Enode Link session for the user
-    const clientGrant = await client.grant({
-      grant_type: "client_credentials",
-    });
-    const body: LinkUserResponseBody = await got
-      .post(`${config.get("apiUrl")}/users/${user.id}/link`, {
-        headers: {
-          Authorization: `Bearer ${clientGrant.access_token}`,
-        },
-        json: {
-          userName: `${user.firstName} ${user.lastName}`,
-          userImage: user.image,
-          linkMultiple: false,
-        },
-      })
-      .json();
-    const linkState = body.linkState;
-
-    // Persist linkState to user's session
-    req.session!.linkState = linkState;
-
-    // Construct an OAuth authorization URL
-    const authorizationUrl = client.authorizationUrl({
-      scope: "offline_access all control:vehicle:charging",
-      state: linkState,
+  console.log("Config:", config);
+  try {
+    // Configure OAuth client library
+    const enodeIssuer = await Issuer.discover(config.get("oauthUrl"));
+    const client = new enodeIssuer.Client({
+      client_id: config.get("clientId"),
+      client_secret: config.get("clientSecret"),
+      redirect_uris: [config.get("callbackUrl")],
+      response_types: ["code"],
     });
 
-    // Redirect user to authorization URL
-    res.redirect(authorizationUrl);
-  });
+    app.get("/link", async (req, res) => {
+      const user = req.session!.user;
 
-  app.get("/callback", async (req, res) => {
-    // Fetch linkState previously store in user session
-    const linkState = req.session!.linkState;
+      // Create an Enode Link session for the user
+      const clientGrant = await client.grant({
+        grant_type: "client_credentials",
+      });
+      const body: LinkUserResponseBody = await got
+        .post(`${config.get("apiUrl")}/users/${user.id}/link`, {
+          headers: {
+            Authorization: `Bearer ${clientGrant.access_token}`,
+          },
+          json: {
+            userName: `${user.firstName} ${user.lastName}`,
+            userImage: user.image,
+            linkMultiple: false,
+          },
+        })
+        .json();
+      const linkState = body.linkState;
 
-    // Use authorization code to obtain an access_token and refresh_token
-    try {
-      const params = client.callbackParams(req);
-      const tokenSet = await client.oauthCallback(
-        config.get("callbackUrl"),
-        params,
-        { state: linkState },
-      );
-      req.session!.tokens = tokenSet;
-    } catch (e) {
-      req.session!.tokens = e;
-    }
-    res.redirect("/");
-  });
+      // Persist linkState to user's session
+      req.session!.linkState = linkState;
 
-  app.post("/refresh", async (req, res) => {
-    // Use refresh_token to obtain a new access_token and refresh_token
-    const refreshToken = req.body.refreshToken;
-    try {
-      const tokenSet = await client.refresh(refreshToken);
-      req.session!.tokens = tokenSet;
-    } catch (e) {
-      req.session!.tokens = e;
-    }
-    res.redirect("/");
-  });
+      // Construct an OAuth authorization URL
+      const authorizationUrl = client.authorizationUrl({
+        scope: "offline_access all control:vehicle:charging",
+        state: linkState,
+      });
 
-  app.listen(config.get("port"), () =>
-    console.log(`Server ready on port ${config.get("port")}`),
-  );
+      console.log("LINK REDIRECT", authorizationUrl);
+
+      // Redirect user to authorization URL
+      res.redirect(authorizationUrl);
+    });
+
+    app.get("/callback", async (req, res) => {
+      // Fetch linkState previously store in user session
+      const linkState = req.session!.linkState;
+
+      // Use authorization code to obtain an access_token and refresh_token
+      try {
+        const params = client.callbackParams(req);
+        const tokenSet = await client.oauthCallback(
+          config.get("callbackUrl"),
+          params,
+          { state: linkState },
+        );
+        req.session!.tokens = tokenSet;
+      } catch (e) {
+        req.session!.tokens = e;
+      }
+      res.redirect("/");
+    });
+
+    app.post("/refresh", async (req, res) => {
+      // Use refresh_token to obtain a new access_token and refresh_token
+      const refreshToken = req.body.refreshToken;
+      try {
+        const tokenSet = await client.refresh(refreshToken);
+        req.session!.tokens = tokenSet;
+      } catch (e) {
+        req.session!.tokens = e;
+      }
+      res.redirect("/");
+    });
+
+    app.listen(config.get("port"), () =>
+      console.log(`Server ready on port ${config.get("port")}`),
+    );
+  } catch (err) {
+    console.log("ERROR", err);
+  }
 })();
